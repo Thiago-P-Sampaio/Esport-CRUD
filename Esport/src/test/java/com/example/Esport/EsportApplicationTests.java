ee4de4dd084a2bb927a192e3fd1d6a39371aa104
TThiagguim07 <thiago.p.sampaio07@gmail.com>
2024-10-18 10:35:48 -0300

    Add files via upload

---
+++ Esport/src/test/java/com/example/Esport/EsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Esport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
